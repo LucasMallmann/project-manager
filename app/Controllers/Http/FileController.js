d2acc3b616e7568fb9b60ff7c3fdb932049189d4
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-06-17 21:12:10 -0300

    Hook para envio de e-mail adicionado

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -20,35 +20,27 @@ class FileController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
-    try {
-      if (!request.file('file')) return
-      const upload = request.file('file', { size: '2mb' })
+    if (!request.file('file')) return
+    const upload = request.file('file', { size: '2mb' })
 
-      const fileName = `${Date.now()}.${upload.subtype}`
+    const fileName = `${Date.now()}.${upload.subtype}`
 
-      await upload.move(Helpers.tmpPath('uploads'), {
-        name: fileName
-      })
+    await upload.move(Helpers.tmpPath('uploads'), {
+      name: fileName
+    })
 
-      if (!upload.moved()) {
-        throw upload.error()
-      }
-
-      const file = await File.create({
-        file: fileName,
-        name: upload.clientName,
-        type: upload.type,
-        subtype: upload.subtype
-      })
-
-      return file
-    } catch (error) {
-      return response.status(error.status).send({
-        error: {
-          message: 'Erro no upload de arquivos'
-        }
-      })
+    if (!upload.moved()) {
+      throw upload.error()
     }
+
+    const file = await File.create({
+      file: fileName,
+      name: upload.clientName,
+      type: upload.type,
+      subtype: upload.subtype
+    })
+
+    return file
   }
 
   /**
